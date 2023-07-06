a0e72842d42db5a5db27c571f25d0d59a025cb02
JoshuaUmbac <joshuarheyumbac14@gmail.com>
2023-07-06 22:45:32 +0800

    refactor: landing page component

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,13 +1,8 @@
 <template>
-    <div class="flex flex-col justify-center items-center h-full">
-        <h1 class="text-3xl text-on-ground">Welcome to Task Planner</h1>
-        <p class="text-on-ground mt-4 text-sm w-64 text-center">Get things done with your day, a list that refreshes every day.</p>
-        <AddTask class="mt-16" placeholder="What's your plan for today?" :focus-text="focusText"/>
-    </div>
+    <LandingPage />
 </template>
 
 <script setup>
-const focusText = 'Try typing "Submit report by Friday 3 PM"'
 
 </script>
 
