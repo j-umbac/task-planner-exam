c8aacf9da61ad1a3d40b82b866efebf66bcab60d
JoshuaUmbac <joshuarheyumbac14@gmail.com>
2023-07-06 22:45:02 +0800

    feat: add task functionality

--- src/components/add-task.vue
+++ src/components/add-task.vue
@@ -3,7 +3,8 @@
         <form @submit.prevent="handleSubmit()">
             <div class="flex p-2 w-full space-x-4 items-center bg-surface-input rounded">
                 <Icon name="fa6-solid:plus" color="gray"></Icon>
-                <input class="w-full outline-0 bg-surface-input" type="text" :placeholder="placeholderText" @focus="updateFocus()" @focusout="revertFocus()">
+                <input class="w-full outline-0 bg-surface-input" type="text" :placeholder="placeholderText" @focus="updateFocus()" @focusout="revertFocus()"
+                v-model="newTask.title">
             </div>
             <small class="text-on-surface">Press <span>enter</span> to add task</small>
         </form>
@@ -13,6 +14,11 @@
 <script setup>
 const props = defineProps(['placeholder', 'focus-text'])
 const placeholderText = ref(props.placeholder)
+const newTask = ref({
+    title: '',
+    is_done: false,
+    is_important: false
+})
 
 const updateFocus = () => {
     if (props.focusText){
@@ -24,8 +30,13 @@ const revertFocus = () => {
     placeholderText.value = props.placeholder
 }
 
-const handleSubmit = () => {
-    console.log("Task Submitted")
+const handleSubmit = async () => {
+    console.log("Processing", newTask.value)
+    await useFetch(useRuntimeConfig().public.apiUrl + '/tasks', {
+        method: 'POST',
+        headers: {'content-type':'application/json'},
+        body: JSON.stringify(newTask.value)
+    })
     navigateTo({ name: "tasks" })
 }
 
