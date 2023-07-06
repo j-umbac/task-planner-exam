96c87d8ee1c94b31da7b803020375d7d1bedc79b
JoshuaUmbac <joshuarheyumbac14@gmail.com>
2023-07-06 22:45:10 +0800

    feat: filter buttons

---
+++ src/components/filter-buttons.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="flex space-x-2 text-on-surface">
+        <button class="px-4 py-1 rounded-md" :class="{'bg-surface-active': activeFilter == 'all'}" @click="handleFilter('all')">
+           <span></span> All
+        </button>
+        <button class="px-4 py-1 rounded-md" :class="{'bg-surface-active': activeFilter == 'important'}" @click="handleFilter('important')">
+            <span>{{ importantLength }}</span> Important
+        </button>
+        <button class="px-4 py-1 rounded-md" :class="{'bg-surface-active': activeFilter == 'done'}" @click="handleFilter('done')">
+           <span>{{ doneLength }}</span> Done
+        </button>
+    </div>
+</template>
+
+<script setup>
+const emit = defineEmits(['useFilter'])
+import { useTaskStore } from '@/stores/states'
+const tasks = useTaskStore()
+
+const activeFilter = ref('all')
+const { getFilteredTasks } = useTasks()
+
+let importantLength = tasks.taskList.filter(task => task.is_important == true).length
+
+let doneLength = tasks.taskList.filter(task => (task.is_done == true) || (task.is_done == 'Completed')).length
+
+const handleFilter = (filter) => {
+    activeFilter.value = filter
+    getFilteredTasks(filter)
+    emit('useFilter', filter)
+}
+
+</script>
+
+<style lang="scss" scoped></style>
