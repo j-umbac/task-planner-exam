6622de8ce7aff84e5447dce4ffe91adb3d10ecf0
JoshuaUmbac <joshuarheyumbac14@gmail.com>
2023-07-06 14:00:41 +0800

    feat: add tasks list component

---
+++ src/components/tasks-list.vue
@@ -0,0 +1,22 @@
+<template>
+    <div v-if="pending">
+        Loading...
+    </div>
+    <div v-else>
+        <ul class="space-y-4">
+            <li v-for="task in tasks" :key="task.id">
+                <!-- Task Component -->
+                <Task :task="task"/>
+                {{ task }}
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script setup>
+const { pending, data: tasks } = await useFetch('https://63906a8d65ff41831112cdd1.mockapi.io/api/v1/tasks', {
+    lazy: true
+})
+</script>
+
+<style lang="scss" scoped></style>
