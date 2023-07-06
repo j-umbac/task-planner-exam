5488878f3df5b32c9cc840bd79690dfd72adc707
JoshuaUmbac <joshuarheyumbac14@gmail.com>
2023-07-06 22:51:38 +0800

    fix: tasks and filtered tasks render

--- src/components/tasks-list.vue
+++ src/components/tasks-list.vue
@@ -1,22 +1,29 @@
 <template>
-    <div v-if="pending">
+    <div v-if="!tasks.taskList">
         Loading...
     </div>
-    <div v-else>
-        <ul class="space-y-4">
-            <li v-for="task in tasks" :key="task.id">
-                <!-- Task Component -->
+    <div v-else class="mt-4">
+        <ul class="space-y-4" v-if="filtered">
+            <li v-for="task in tasks.filteredTaskList " :key="task.id">
                 <Task :task="task"/>
-                {{ task }}
+            </li>
+        </ul>
+        <ul class="space-y-4" v-else>
+            <li v-for="task in tasks.taskList" :key="task.id">
+                <Task :task="task"/>
             </li>
         </ul>
     </div>
 </template>
 
 <script setup>
-const { pending, data: tasks } = await useFetch('https://63906a8d65ff41831112cdd1.mockapi.io/api/v1/tasks', {
-    lazy: true
-})
+const { filtered } = defineProps(['filtered'])
+import { useTaskStore } from '@/stores/states'
+const { getTasks} = useTasks()
+
+getTasks()
+const tasks = useTaskStore()
+
 </script>
 
 <style lang="scss" scoped></style>
