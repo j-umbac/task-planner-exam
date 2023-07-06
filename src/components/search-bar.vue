e2609bb16a2eaf2a796d326658c84224116f95f6
JoshuaUmbac <joshuarheyumbac14@gmail.com>
2023-07-06 22:47:25 +0800

    feat: searchbar component

---
+++ src/components/search-bar.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="flex p-2 w-full space-x-4 items-center bg-surface-card rounded-full shadow-inner text-on-card">
+        <Icon name="fa6-solid:magnifying-glass" class="ml-2"></Icon>
+        <input class="w-full outline-0 bg-surface-card" type="text" v-model="searchQuery" placeholder="Search"
+            @keyup="search(searchQuery, timer)">
+    </div>
+</template>
+
+<script setup>
+const { filtered } = defineProps(['filtered'])
+const { searchTasks } = useTasks()
+
+const searchQuery = ref('')
+const timer = ref(null)
+
+const search = () => {
+    if (timer.value) clearTimeout(timer.value)
+    timer.value = setTimeout(() => {
+        searchTasks(searchQuery.value)
+    }, 500)
+}
+
+</script>
+
+<style lang="scss" scoped></style>
