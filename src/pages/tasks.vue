a489e0674fefaf3d83164bfb4d476c80697e99f9
JoshuaUmbac <joshuarheyumbac14@gmail.com>
2023-07-06 22:52:31 +0800

    feat: add components

--- src/pages/tasks.vue
+++ src/pages/tasks.vue
@@ -1,14 +1,37 @@
 <template>
     <div class="flex flex-col h-[100dvh] container mx-auto md:w-auto w-[90%]">
-        <h1 class="text-3xl font-semibold text-on-ground mt-16">Task Planner</h1>
+        <div class="flex mt-16 justify-between items-center">
+            <h1 class="text-2xl font-semibold text-on-ground">Task Planner</h1>
+            <FilterButtons @useFilter="handleFilter" />
+        </div>
         <div class="flex flex-col h-full mt-4 justify-between pb-8">
-            <TasksList />
-            <AddTask placeholder="Add a Task"/>
+            <div>
+                <SearchBar :filtered="filtered" />
+                <TasksList :filtered="filtered" />
+            </div>
+            <ul>
+                <li v-for="task in tasks.taskList" class="border p-2 m-2">
+                    {{ task }}
+                </li>
+            </ul>
+            <AddTask placeholder="Add a Task" />
         </div>
     </div>
 </template>
 
 <script setup>
+import { useTaskStore } from '@/stores/states'
+const tasks = useTaskStore()
+const filtered = ref(false)
+
+const handleFilter = (filter) => {
+    if (filter == 'all') {
+        filtered.value = false
+    } else [
+        filtered.value = true
+    ]
+}
+
 </script>
 
 <style lang="scss" scoped></style>
