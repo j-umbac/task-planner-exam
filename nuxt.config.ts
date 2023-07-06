487273b09914c5cf680e145885b3ebd144eb2403
JoshuaUmbac <joshuarheyumbac14@gmail.com>
2023-07-06 11:18:24 +0800

    build: nuxt build

---
+++ nuxt.config.ts
@@ -0,0 +1,40 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+
+  srcDir: './src',
+
+  modules: [
+    [
+      '@nuxtjs/tailwindcss', {
+        cssPath: '~/assets/styles/tailwind.css'
+      }
+    ],
+  ],
+
+  css: ['~/assets/css/main.scss'],
+
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+
+  runtimeConfig: {
+    // Keys within public are also exposed client-side
+    public: {
+      environment: process.env.APP_ENV,
+      baseUrl: process.env.BASE_URL,
+      apiUrl: process.env.API_URL,
+      version: process.env.VERSION,
+    }
+  },
+
+  app: {
+    head: {
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
+    }
+  }
+})
