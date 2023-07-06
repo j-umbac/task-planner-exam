eafcfc5037fa39d0895316cea388a0453447f1a0
JoshuaUmbac <joshuarheyumbac14@gmail.com>
2023-07-06 22:50:52 +0800

    fix: task functionalities

--- src/components/task.vue
+++ src/components/task.vue
@@ -1,24 +1,29 @@
 <template>
-    <div class="bg-surface-card p-4 shadow-lg rounded-md text-on-card flex items-center space-x-2">
-        <Icon :name="isDoneIcon" @click="toggleIcon()"/>
-        <span :class="{'line-through': task.is_done}">
-            {{ task.title }}
-        </span>
+    <div class="bg-surface-card p-4 shadow-lg rounded-md text-on-card flex items-center justify-between">
+        <div class="flex items-center space-x-2">
+            <button @click="toggleComplete(task)">
+                <Icon v-if="task.is_done" name="fa6-solid:circle-check" />
+                <Icon v-else name="fa6-solid:circle" />
+            </button>
+            <span :class="{ 'line-through': task.is_done }">
+                {{ task.title }}
+            </span>
+        </div>
+        <div class="flex space-x-2">
+            <button @click="toggleImportant(task)">
+                <Icon v-if="task.is_important" name="fa6-solid:star" color="#F6C23E" />
+                <Icon v-else name="fa6-regular:star" />
+            </button>
+            <button @click="deleteTask(task.id)">
+                <Icon name="fa6-solid:trash-can" class="hover:text-red-600"></Icon>
+            </button>
+        </div>
     </div>
 </template>
 
 <script setup>
 const { task } = defineProps(['task'])
-
-const isDoneIcon = ref('fa6-solid:circle')
-
-if (task.is_done===('Completed' || true)) {
-    isDoneIcon.value = 'fa6-solid:circle-check'
-}
-
-const toggleIcon = () => {
-    task.is_done = !task.is_done
-}
+const { deleteTask, toggleImportant, toggleComplete } = useTask()
 
 </script>
 
