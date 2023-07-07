9cb7b0dfc172c03fbc091b2a85c965d5cb5bf422
JoshuaUmbac <joshuarheyumbac14@gmail.com>
2023-07-07 23:30:05 +0800

    ui: edit task

--- src/components/edit-task.vue
+++ src/components/edit-task.vue
@@ -1,14 +1,15 @@
 <template>
-    <div class="flex justify-between items-center">
-        Edit Task {{ task.title }}
-        <input type="text" v-model="task.title">
-        <div class="flex space-x-2 mr-2">
-            <button class="hover:bg-gray-100 px-1 rounded-md pb-1 text-red-600">
-                <Icon name="fa6-solid:xmark" />
-            </button>
-            <button class="hover:bg-gray-100 px-1 rounded-md pb-1 text-green-600">
-                <Icon name="fa6-solid:check" />
-            </button>
+    <div class="flex justify-between items-center space-x-2">
+        <div class="flex justify-between py-1 px-2 w-full rounded-md shadow-inner text-on-card border mr-2">
+            <input type="text" v-model="task.title">
+            <div class="flex space-x-2">
+                <button class="hover:bg-gray-100 px-1 rounded-md pb-1 text-red-600" @click="$emit('closeEdit')">
+                    <Icon name="fa6-solid:xmark" />
+                </button>
+                <button class="hover:bg-gray-100 px-1 rounded-md pb-1 text-green-600">
+                    <Icon name="fa6-solid:check" />
+                </button>
+            </div>
         </div>
     </div>
 </template>
