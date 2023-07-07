ca4858e5df9d6b2137ef09fe89ad67d9fb703f2e
JoshuaUmbac <joshuarheyumbac14@gmail.com>
2023-07-07 14:12:53 +0800

    feat: add API calls

--- src/composables/use-task.js
+++ src/composables/use-task.js
@@ -13,15 +13,48 @@ export function useTask() {
 
     async function toggleImportant(task) {
         task.is_important = !task.is_important
+        await useFetch(useRuntimeConfig().public.apiUrl + '/tasks/' + task.id, {
+            method: 'PUT',
+            headers: { 'content-type': 'application/json' },
+            body: JSON.stringify({'is_important': task.is_important})
+        }).catch(err => {
+            console.log(err)
+        })
+        tasks.importantLength = tasks.taskList.filter(task => task.is_important == true).length
     }
 
     async function toggleComplete(task) {
         task.is_done = !task.is_done
+        await useFetch(useRuntimeConfig().public.apiUrl + '/tasks/' + task.id, {
+            method: 'PUT',
+            headers: { 'content-type': 'application/json' },
+            body: JSON.stringify({'is_done': task.is_done})
+        }).catch(err => {
+            console.log(err)
+        })
+        tasks.doneLength = tasks.taskList.filter(task => (task.is_done == true) || (task.is_done == 'Completed')).length
+    }
+
+    async function updateAssignee(task, assignee) {
+        task.assignee = assignee
+        await useFetch(useRuntimeConfig().public.apiUrl + '/tasks/' + task.id, {
+            method: 'PUT',
+            headers: { 'content-type': 'application/json' },
+            body: JSON.stringify({'assignee': task.assignee})
+        }).catch(err => {
+            console.log(err)
+        })
+    }
+
+    async function editTask(task) {
+        console.log(task)
     }
 
     return {
         deleteTask,
         toggleImportant,
-        toggleComplete
+        toggleComplete,
+        updateAssignee,
+        editTask
     }
 }
