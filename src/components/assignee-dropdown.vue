2ec920ebc471ba1f4ad8c06da32b09bcd8ae5632
JoshuaUmbac <joshuarheyumbac14@gmail.com>
2023-07-07 14:14:04 +0800

    feat: assignee components

---
+++ src/components/assignee-dropdown.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="border shadow p-2 rounded-md absolute bg-surface-card w-auto">
+         <AssigneeSearchBar />
+         <div class="my-2 h-64 overflow-y-scroll">
+            <ul class="flex flex-col space-y-2 px-2">
+                <li v-for="user in tasks.users" :key="user.id" @click="updateAssignee(task, user)" class="hover:cursor-pointer hover:bg-gray-100 p-1 rounded-md">
+                    <div class="flex items-center space-x-2">
+                        <nuxt-img :src="user.avatar" width="24px" height="24px" class="rounded-full"/>
+                        <span class="text-sm">{{ user.name }}</span>
+                    </div>
+                </li>
+            </ul>
+         </div>
+    </div>
+</template>
+
+<script setup>
+const { task } = defineProps(['task'])
+import { useTaskStore } from '@/stores/states'
+const { updateAssignee } = useTask()
+
+const tasks = useTaskStore()
+
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/assignee-icon.vue
@@ -0,0 +1,32 @@
+<template>
+    <div v-if="Object.keys(assignee).length" ref="target">
+        <button @click="toggleDropDown(); toggle = !toggle" >
+            <nuxt-img :src="assignee.avatar" width="24px" height="24px" class="rounded-full" />
+        </button>
+        <AssigneeDropdown v-if="toggle" :task="task"/>
+    </div>
+    <div v-else ref="target">
+        <button class="hover:bg-gray-100 px-1 rounded-md pb-1" @click="toggleDropDown(); toggle = !toggle">
+            <Icon name="fa6-solid:angle-up" v-if="toggle" />
+            <Icon name="fa6-solid:angle-down" v-else />
+        </button>
+        <AssigneeDropdown v-if="toggle" :task="task"/>
+    </div>
+</template>
+
+<script setup>
+const { assignee, task } = defineProps(['assignee', 'task'])
+const toggle = ref(false)
+const target = ref(null)
+
+
+const toggleDropDown = () => {
+    console.log(toggle.value)
+    onClickOutside(target, () => {
+        toggle.value = false
+    })
+}
+
+</script>
+
+<style lang="scss" scoped></style>
