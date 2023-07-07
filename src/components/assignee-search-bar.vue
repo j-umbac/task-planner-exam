15d0f1cd991e90c3f81691d14c4d49e793899bec
JoshuaUmbac <joshuarheyumbac14@gmail.com>
2023-07-07 14:13:52 +0800

    ui: assignee bar styling

---
+++ src/components/assignee-search-bar.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="flex p-1 w-full space-x-4 items-center bg-surface-card rounded-md border text-on-card">
+        <Icon name="fa6-solid:magnifying-glass" class="ml-1"></Icon>
+        <input class="w-full outline-0 bg-surface-card text-sm" type="text" v-model="searchQuery" placeholder="Search Team Member" @keyup="search()">
+    </div>
+</template>
+
+<script setup>
+const searchQuery = ref('')
+const timer = ref(null)
+const { searchUsers } = useUsers()
+
+const search = () => {
+    if (timer.value) clearTimeout(timer.value)
+    timer.value = setTimeout(() => {
+        searchUsers(searchQuery.value)
+    }, 500)
+}
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/components/search-bar.vue
+++ src/components/search-bar.vue
@@ -1,8 +1,8 @@
 <template>
-    <div class="flex p-2 w-full space-x-4 items-center bg-surface-card rounded-full shadow-inner text-on-card">
+    <div class="flex p-2 w-full space-x-4 items-center bg-surface-card rounded-full shadow-inner text-on-card border">
         <Icon name="fa6-solid:magnifying-glass" class="ml-2"></Icon>
         <input class="w-full outline-0 bg-surface-card" type="text" v-model="searchQuery" placeholder="Search"
-            @keyup="search(searchQuery, timer)">
+            @keyup="search()">
     </div>
 </template>
 
