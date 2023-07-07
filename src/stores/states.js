793ba68b3f40f1b70fbfb1eef1be2648b2dd6401
JoshuaUmbac <joshuarheyumbac14@gmail.com>
2023-07-07 14:13:20 +0800

    feat: add states

--- src/stores/states.js
+++ src/stores/states.js
@@ -2,6 +2,12 @@
 export const useTaskStore = defineStore('tasks', () => {
     const taskList = ref([])
     const filteredTaskList = ref([])
+    const users = ref([])
+    const filteredUsers = ref([])
 
-    return { taskList, filteredTaskList }
+    const importantLength = ref(0)
+
+    const doneLength = ref(0)
+
+    return { taskList, filteredTaskList, users, filteredUsers, importantLength, doneLength }
 })
