5163b94ce0e10f1232df0dd9e2792ddc329e670d
JoshuaUmbac <joshuarheyumbac14@gmail.com>
2023-07-08 00:01:32 +0800

    feat: edit task

--- src/components/edit-task.vue
+++ src/components/edit-task.vue
@@ -1,15 +1,18 @@
 <template>
     <div class="flex justify-between items-center space-x-2">
-        <div class="flex justify-between py-1 px-2 w-full rounded-md shadow-inner text-on-card border mr-2">
-            <input type="text" v-model="task.title">
-            <div class="flex space-x-2">
+        <div class="flex justify-between py-1 px-2 w-full rounded-md shadow-inner text-on-card border mr-2" ref="target">
+            <input type="text" v-model="task.title" class="w-full outline-0">
+            <button class="hover:bg-gray-100 px-1 rounded-md pb-1 text-green-600" @click="editTask(task); $emit('closeEdit')">
+                    <Icon name="fa6-solid:check" />
+                </button>
+            <!-- <div class="flex space-x-2">
                 <button class="hover:bg-gray-100 px-1 rounded-md pb-1 text-red-600" @click="$emit('closeEdit')">
                     <Icon name="fa6-solid:xmark" />
                 </button>
                 <button class="hover:bg-gray-100 px-1 rounded-md pb-1 text-green-600">
                     <Icon name="fa6-solid:check" />
                 </button>
-            </div>
+            </div> -->
         </div>
     </div>
 </template>
@@ -17,6 +20,14 @@
 <script setup>
 const { task } = defineProps(['task'])
 const emit = defineEmits(['closeEdit'])
+const { editTask } = useTask()
+
+const target = ref(null)
+
+onClickOutside(target, () => {
+    editTask(task)
+    emit('closeEdit')
+})
 
 </script>
 
