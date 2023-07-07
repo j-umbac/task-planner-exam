2d0b563c6cfc9fa15802726e1b69514d6fdc7c30
JoshuaUmbac <joshuarheyumbac14@gmail.com>
2023-07-07 23:07:28 +0800

    feat: edit task component

---
+++ src/components/edit-task.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="flex justify-between items-center">
+        Edit Task {{ task.title }}
+        <input type="text" v-model="task.title">
+        <div class="flex space-x-2 mr-2">
+            <button class="hover:bg-gray-100 px-1 rounded-md pb-1 text-red-600">
+                <Icon name="fa6-solid:xmark" />
+            </button>
+            <button class="hover:bg-gray-100 px-1 rounded-md pb-1 text-green-600">
+                <Icon name="fa6-solid:check" />
+            </button>
+        </div>
+    </div>
+</template>
+
+<script setup>
+const { task } = defineProps(['task'])
+const emit = defineEmits(['closeEdit'])
+
+</script>
+
+<style lang="scss" scoped></style>
