0803b8354eb9934186685a632bcd879b141275ed
JoshuaUmbac <joshuarheyumbac14@gmail.com>
2023-07-08 16:07:15 +0800

    feat: dashboard layout

---
+++ src/layouts/dashboard.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="bg-surface-ground min-h-[100dvh]">
+        <DashboardHeader />
+        <main class="h-full container mx-auto">
+            <slot />
+        </main>
+    </div>
+</template>
