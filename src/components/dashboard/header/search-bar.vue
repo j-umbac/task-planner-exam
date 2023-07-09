2522ed6a5dc0073da6a57e3ebe66cc25ab94c44f
JoshuaUmbac <joshuarheyumbac14@gmail.com>
2023-07-09 11:08:24 +0800

    ui: search bar width

--- src/components/dashboard/header/search-bar.vue
+++ src/components/dashboard/header/search-bar.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="flex justify-between p-1 w-full space-x-4 items-center bg-surface-card rounded-full text-on-card border">
+    <div class="flex justify-between p-1 w-4/5 space-x-4 items-center bg-surface-card rounded-full text-on-card border">
         <input class="w-full outline-0 bg-surface-card px-2 font-light" type="text" v-model="searchQuery"
             placeholder="Type for job title or role">
         <button class="flex space-x-2 items-center rounded-full bg-dashboard-primary text-on-primary py-2 px-6">
