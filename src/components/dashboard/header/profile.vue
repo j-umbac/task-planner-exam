a4f5917cc25237041e38269cb8de1f30c394cc5b
JoshuaUmbac <joshuarheyumbac14@gmail.com>
2023-07-09 14:36:32 +0800

    fix: icon and text spacing

--- src/components/dashboard/header/profile.vue
+++ src/components/dashboard/header/profile.vue
@@ -1,11 +1,11 @@
 <template>
     <div class="flex space-x-3 items-center">
         <div class="text-right">
-            <h4 class="font-semibold">{{ user.name }}</h4>
-            <h5 class="text-dashboard-secondary">{{ user.email }}</h5>
+            <h4 class="font-semibold text-normal">{{ user.name }}</h4>
+            <h5 class="text-dashboard-secondary text-sm">{{ user.email }}</h5>
         </div>
         <div class="flex space-x-1">
-            <nuxt-img :src="user.avatar" class="w-auto h-10"/>
+            <nuxt-img :src="user.avatar" class="w-auto h-8"/>
             <button class="px-2 py-1 hover:bg-gray-100 rounded-md">
                 <Icon name="fa6-solid:angle-down" />
             </button>
