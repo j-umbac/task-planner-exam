a4038b7facb3eacc992405a3869de351904394b3
JoshuaUmbac <joshuarheyumbac14@gmail.com>
2023-07-09 14:39:11 +0800

    fix: dashboard layout and components

--- src/pages/dashboard.vue
+++ src/pages/dashboard.vue
@@ -1,20 +1,21 @@
 <template>
-    <div class="grid grid-cols-12 gap-2 mt-8">
-        <div class="col-span-3 row-span-3 mt-8">
-            <DashboardSidebar />
-        </div>
-        <div class="col-span-9">
+    <div class="grid grid-cols-12 gap-6 mt-8">
+        <div class="col-span-12 col-start-1">
             <div class="rounded bg-white shadow">
                 <DashboardWelcomeCard />
                 <div class="border-t"></div>
                 <DashboardProgress />
             </div>
         </div>
-        <div class="col-start-4 col-span-6 row-span-2">
+        <div class="col-span-8">
             <DashboardMainPanel class="rounded bg-white shadow" />
         </div>
-        <div class="col-start-10 col-span-3 border h-32"></div>
-        <div class="col-start-10 col-span-3 border h-32"></div>
+        <div class="col-span-4 space-y-6">
+            <DashboardVideo class="rounded bg-white shadow" />
+            <DashboardProfileProgress :progress="100" class="rounded bg-white shadow" />
+            <DashboardProfileProgress :progress="50" class="rounded bg-white shadow" />
+
+        </div>
     </div>
 </template>
 
