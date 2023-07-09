75f518a15cadf51a286a937eab61c1a83ad0efb9
JoshuaUmbac <joshuarheyumbac14@gmail.com>
2023-07-09 11:08:36 +0800

    dashboard components

--- src/pages/dashboard.vue
+++ src/pages/dashboard.vue
@@ -1,13 +1,19 @@
 <template>
     <div class="grid grid-cols-12 gap-2 mt-8">
-        <div class="col-span-3 row-span-4">
+        <div class="col-span-3 row-span-3 mt-8">
             <DashboardSidebar />
         </div>
         <div class="col-span-9">
-            <DashboardWelcomeCard class="rounded bg-white shadow"/>
-            <DashboardProgress class="rounded bg-white shadow mt-1"/>
+            <div class="rounded bg-white shadow">
+                <DashboardWelcomeCard />
+                <div class="border-t"></div>
+                <DashboardProgress />
+            </div>
         </div>
-        <div class="col-start-4 col-span-6 border h-32"></div>
+        <div class="col-start-4 col-span-6 row-span-2">
+            <DashboardMainPanel class="rounded bg-white shadow" />
+        </div>
+        <div class="col-start-10 col-span-3 border h-32"></div>
         <div class="col-start-10 col-span-3 border h-32"></div>
     </div>
 </template>
@@ -15,10 +21,8 @@
 <script setup>
 definePageMeta({
     layout: 'dashboard'
-  })
+})
 
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
