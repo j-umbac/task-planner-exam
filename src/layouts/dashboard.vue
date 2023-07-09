cc263096e5ab84e636518dbbe2f7566d6358a38f
JoshuaUmbac <joshuarheyumbac14@gmail.com>
2023-07-09 14:38:54 +0800

    fix: dashboard layout

--- src/layouts/dashboard.vue
+++ src/layouts/dashboard.vue
@@ -1,8 +1,14 @@
 <template>
-    <div class="bg-surface-ground min-h-[100dvh]">
-        <DashboardHeader />
-        <main class="h-full container mx-auto">
-            <slot />
+    <div class="bg-dashboard-ground min-h-[100dvh]">
+        <DashboardHeader class="sticky z-20 top-0" />
+        <main class="h-full container mx-auto flex text-normal">
+            <div class="basis-1/4 mr-6">
+                <DashboardSidebar class="sticky top-32"/>
+            </div>
+            <div class="basis-3/4 mb-8">
+                <slot />
+            </div>
         </main>
+
     </div>
 </template>
