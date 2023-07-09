93bdbf3ed29cf6639a243f266867f7dce25b35a0
JoshuaUmbac <joshuarheyumbac14@gmail.com>
2023-07-09 14:36:46 +0800

    fix: search bar spacing

--- src/components/dashboard/header/search-bar.vue
+++ src/components/dashboard/header/search-bar.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="flex justify-between p-1 w-4/5 space-x-4 items-center bg-surface-card rounded-full text-on-card border">
-        <input class="w-full outline-0 bg-surface-card px-2 font-light" type="text" v-model="searchQuery"
+        <input class="w-full outline-0 bg-surface-card px-8 font-light" type="text" v-model="searchQuery"
             placeholder="Type for job title or role">
-        <button class="flex space-x-2 items-center rounded-full bg-dashboard-primary text-on-primary py-2 px-6">
+        <button class="flex space-x-2 items-center rounded-full bg-dashboard-primary text-on-primary py-1 px-4 text-normal">
             <Icon name="fa6-solid:magnifying-glass" />
             <span>Search</span>
         </button>
